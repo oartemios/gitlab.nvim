d172c1f67eb3feb61d19f75265fd6c8ef5da08d3
Орлов Артем Геннадьевич <a.g.orlov@tinkoff.ru>
2023-12-21 13:08:06 +0700

    feat(support-port-in-repo-url): extended regular expression to support port in project URL

--- cmd/git.go
+++ cmd/git.go
@@ -41,18 +41,19 @@ func extractGitInfo(refreshGitInfo func() error, getProjectRemoteUrl func() (str
 	}
 
 	/*
-	   This should match following formats:
-	       namespace: namespace, projectName: dummy-test-repo:
-	           https://gitlab.com/namespace/dummy-test-repo.git
-	           git@gitlab.com:namespace/dummy-test-repo.git
-	           ssh://git@gitlab.com/namespace/dummy-test-repo.git
+				   This should match following formats:
+				       namespace: namespace, projectName: dummy-test-repo:
+				           https://gitlab.com/namespace/dummy-test-repo.git
+				           git@gitlab.com:namespace/dummy-test-repo.git
+				           ssh://git@gitlab.com/namespace/dummy-test-repo.git
+		               ssh://git@gitlab.com:8888/namespace/dummy-test-repo.git
 
-	       namespace: namespace/subnamespace, projectName: dummy-test-repo:
-	           ssh://git@gitlab.com/namespace/subnamespace/dummy-test-repo
-	           https://git@gitlab.com/namespace/subnamespace/dummy-test-repo.git
-	           git@git@gitlab.com:namespace/subnamespace/dummy-test-repo.git
+				       namespace: namespace/subnamespace, projectName: dummy-test-repo:
+				           ssh://git@gitlab.com/namespace/subnamespace/dummy-test-repo
+				           https://git@gitlab.com/namespace/subnamespace/dummy-test-repo.git
+				           git@git@gitlab.com:namespace/subnamespace/dummy-test-repo.git
 	*/
-	re := regexp.MustCompile(`(?:^https?:\/\/|^ssh:\/\/|^git@)(?:[^\/:]+)[\/:](.*)\/([^\/]+?)(?:\.git)?$`)
+	re := regexp.MustCompile(`(?:^https?://|^ssh://|^git@)(?:[^\/:]+)[\/:][0-9/]*(.*)\/([^\/]+?)(?:\.git)?$`)
 	matches := re.FindStringSubmatch(url)
 	if len(matches) != 3 {
 		return GitProjectInfo{}, fmt.Errorf("Invalid Git URL format: %s", url)
